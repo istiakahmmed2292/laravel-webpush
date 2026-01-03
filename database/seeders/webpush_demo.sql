d4f4b6935a46310a94047247f8d4d62103ca2eea
Istiak Ahmmed <istiakahmmed.official@gmail.com>
2026-01-04 02:25:20 +0600

    finally done

--- database/seeders/webpush_demo.sql
+++ database/seeders/webpush_demo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 03, 2026 at 07:18 PM
+-- Generation Time: Jan 03, 2026 at 08:19 PM
 -- Server version: 8.0.44-0ubuntu0.22.04.2
 -- PHP Version: 8.3.29
 
@@ -41,19 +41,7 @@ CREATE TABLE `blogs` (
 --
 
 INSERT INTO `blogs` (`id`, `user_id`, `title`, `content`, `created_at`, `updated_at`) VALUES
-(1, 1, 'fgzdfg', 'dfgdf', '2026-01-03 12:32:33', '2026-01-03 12:32:33'),
-(2, 1, 'ghxfgh', 'xfghfgh', '2026-01-03 12:42:30', '2026-01-03 12:42:30'),
-(3, 1, 'gvhgv', 'uhbh', '2026-01-03 12:58:19', '2026-01-03 12:58:19'),
-(4, 1, 'dgdfg', 'dfgdf', '2026-01-03 13:05:12', '2026-01-03 13:05:12'),
-(5, 1, 'gdfgd', 'fdfgdfg', '2026-01-03 13:05:21', '2026-01-03 13:05:21'),
-(6, 1, 'gdfgdg', 'dgdg', '2026-01-03 13:05:37', '2026-01-03 13:05:37'),
-(7, 1, 'sd', 'sdfs', '2026-01-03 13:05:45', '2026-01-03 13:05:45'),
-(8, 1, 'dgdfg', 'dfgdg', '2026-01-03 13:06:11', '2026-01-03 13:06:11'),
-(9, 1, 'dfgdg', 'erer564fdhg', '2026-01-03 13:08:02', '2026-01-03 13:08:02'),
-(10, 1, 'retertye', 'ereryer', '2026-01-03 13:10:26', '2026-01-03 13:10:26'),
-(11, 1, 'dfgdfgd', 'dgdfgdgd', '2026-01-03 13:10:54', '2026-01-03 13:10:54'),
-(12, 1, 'dfd', 'dfgdfgd', '2026-01-03 13:15:18', '2026-01-03 13:15:18'),
-(13, 1, 'hjcghjghj', 'gfghfghfgh', '2026-01-03 13:16:41', '2026-01-03 13:16:41');
+(1, 2, 'This is new blog', 'Content for New Blog', '2026-01-03 13:33:34', '2026-01-03 13:33:34');
 
 -- --------------------------------------------------------
 
@@ -188,7 +176,8 @@ CREATE TABLE `push_subscriptions` (
 --
 
 INSERT INTO `push_subscriptions` (`id`, `subscribable_type`, `subscribable_id`, `endpoint`, `public_key`, `auth_token`, `content_encoding`, `created_at`, `updated_at`) VALUES
-(2, 'App\\Models\\User', 1, 'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABpWWZ_8dZzqdNppWrm2XOulcffDMp2OUjM0G0AjgLKx-K9CIQjkEdptK2JrWnDNkUmhHf7jlcC2PAOVqjOdRJHTUc-k3ahzgXqvFiiJMCf-_WnqkFFwcDUtorxus5N_UwpZKkLhIKelJbeTmZPowLQj_D19ehp82Ww2LqJe-2eeHzE3yg', 'BPv0CHLdipU7JXHt_SO1ivRyvbjgpyM1pxY60ljvSHIxyUh2_n1KjrPvsBzW30ooFMvU3l38W00IdG7StXbm2NI', 'zQ7sttAD5pJ43lSfjlbr-A', NULL, '2026-01-03 12:57:03', '2026-01-03 12:57:03');
+(1, 'App\\Models\\User', 1, 'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABpWWZ_8dZzqdNppWrm2XOulcffDMp2OUjM0G0AjgLKx-K9CIQjkEdptK2JrWnDNkUmhHf7jlcC2PAOVqjOdRJHTUc-k3ahzgXqvFiiJMCf-_WnqkFFwcDUtorxus5N_UwpZKkLhIKelJbeTmZPowLQj_D19ehp82Ww2LqJe-2eeHzE3yg', 'BPv0CHLdipU7JXHt_SO1ivRyvbjgpyM1pxY60ljvSHIxyUh2_n1KjrPvsBzW30ooFMvU3l38W00IdG7StXbm2NI', 'zQ7sttAD5pJ43lSfjlbr-A', NULL, '2026-01-03 13:30:58', '2026-01-03 13:30:58'),
+(2, 'App\\Models\\User', 2, 'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABpWW7sbqqMncATaWkH1Txn7EEl8sguDnLIdLNabjOueZ-ap8I84n0LFNz8KGjx-PGJuu8BNPvmhaxEqhEuybKGGMnwa-elEP_2zdEabvqxI_QfEaLqzBOFy-P7XndJuduxpJiVLQQNNDLBN1Kkl_1gk96_v8jHX8rqzUVcSM7o_qmp-oA', 'BNKqQnLyt0zHZkbg9nsb26mahfpHh276dinOhEsNfgfLj3TBUmPcpfG1U7Jt74hY3j_5IkuwvyDcgQiEcm2oG_g', 'EhfFWxhcu5oC_GVQDdl-CQ', NULL, '2026-01-03 13:33:00', '2026-01-03 13:33:00');
 
 -- --------------------------------------------------------
 
@@ -210,7 +199,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('KYX2jwwmy9CWDLtNiOgdEb3ywt38HF9GDQ34nAIJ', 1, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:146.0) Gecko/20100101 Firefox/146.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiemZCSFFCRU5FUlVpNGFNN2M1UmN0Z0NyOGJ3NTFUV2RmS2ZBODBUeSI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozNDoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2Jsb2dzL2NyZWF0ZSI7fXM6OToiX3ByZXZpb3VzIjthOjI6e3M6MzoidXJsIjtzOjI3OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYmxvZ3MiO3M6NToicm91dGUiO3M6MTE6ImJsb2dzLmluZGV4Ijt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1767467802);
+('iYieY7hCttdgqGGLl3ECDMSCtKgh61VwckQFijL0', 2, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:146.0) Gecko/20100101 Firefox/146.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiRVBvYVZqbDhQaThDdlVvbkxUTTN3SWRxRDF1M0V4R0F1Nzh4TTZLWCI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2Rhc2hib2FyZCI7fXM6OToiX3ByZXZpb3VzIjthOjI6e3M6MzoidXJsIjtzOjI3OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYmxvZ3MiO3M6NToicm91dGUiO3M6MTE6ImJsb2dzLmluZGV4Ijt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mjt9', 1767468815),
+('wtC6kH44aTrjesZqgHWrR7OC7U5AsTXVnQTCsNxf', 1, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:146.0) Gecko/20100101 Firefox/146.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiZDc1ckc2eTJIaVhiMENIa3NnR2JrcFV0c0J1bmpvNU83UXlrdGh6eCI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czoyNzoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2Jsb2dzIjt9czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO3M6NToicm91dGUiO3M6OToiZGFzaGJvYXJkIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1767468730);
 
 -- --------------------------------------------------------
 
@@ -224,6 +214,7 @@ CREATE TABLE `users` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_admin` tinyint NOT NULL DEFAULT '0' COMMENT '0 = User, 1 = Admin',
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -233,8 +224,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'User', 'user@gmail.com', NULL, '$2y$12$8i1BKfoF8zIxLMbHWJsBG.f74Twfn2TCivj11vfY3pYgNqT0aeFOW', NULL, '2026-01-03 12:32:19', '2026-01-03 12:32:19');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `is_admin`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@gmail.com', NULL, '$2y$12$Aj5yHT4z06dZFJqVYZ3PTeinj9auIR1knzZdiXMnLQ7JwHTLVT0eW', 1, NULL, '2026-01-03 13:30:47', '2026-01-03 13:30:47'),
+(2, 'User', 'user@gmail.com', NULL, '$2y$12$RYZlsm06NbdXyjk4OBs9a..8IRLHIFYPu/RJT3Efl6TB6SdsZqEmK', 0, NULL, '2026-01-03 13:32:53', '2026-01-03 13:32:53');
 
 --
 -- Indexes for dumped tables
@@ -322,7 +314,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `blogs`
 --
 ALTER TABLE `blogs`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -352,7 +344,7 @@ ALTER TABLE `push_subscriptions`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
